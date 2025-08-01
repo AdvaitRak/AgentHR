819cf722102b34efc2a2e2ec7035aa20191efe37
Advait Raktate <168472806+AdvaitRak@users.noreply.github.com>
2025-08-01 17:52:34 +0530

    Update llm.py

--- llm.py
+++ llm.py
@@ -2,10 +2,8 @@ import os
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 
-# Load the .env file containing your GOOGLE_API_KEY
 load_dotenv()
 
-# Initialize the Gemini LLM
 llm = ChatOpenAI(
     base_url="https://api.perplexity.ai",
     api_key=os.getenv("PERPLEXITY_API_KEY"),
